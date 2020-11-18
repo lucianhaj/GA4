358b9d3b14c563bd18445b79d64b55642471734e
Lucian Haj <33040997+lucianhaj@users.noreply.github.com>
2020-11-18 13:08:15 -0800

    Add files via upload

---
+++ assignment4.py
@@ -0,0 +1,21 @@
+'''
+    This file contains the template for Assignment4.  For testing it, I will place it
+    in a different directory, call the function <first_second_third_mst>, and check its output.
+    So, you can add/remove  whatever you want to/from this file.  But, don't change the name
+    of the file or the name/signature of the following function.
+
+    Also, I will use <python3> to run this code.
+'''
+
+def first_second_third_mst(input_file_path, output_file_path):
+    '''
+    This function will contain your code, it will read the input from the file
+    <input_file_path> and write to the file <output_file_path>.
+
+    Params:
+        input_file_path (str): full path of the input file.
+        output_file_path (str): full path of the output file.
+    '''
+    pass
+
+# first_second_third_mst('input0.in', 'input0.out')
